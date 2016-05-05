3aec1ba72db251ce7c76ea84679d82bc5930e1e8
Amal Krishna <amalkrishnat94@gmail.com>
2016-05-05 21:11:21 +0530

    Updated Commit

--- src/main/java/com/wc/util/CountResponse.java
+++ src/main/java/com/wc/util/CountResponse.java
@@ -1,15 +1,28 @@
+/*
+ * CountResponse.java
+ * 
+ * v1.0
+ *
+ * 5 May 2016
+ */
+
 package com.wc.util;
 
 public class CountResponse {
 
-	int count;
-	
-	public int getCount() {
-		return count;
-	}
+ int count;
+ /**  
+  *  Function to return the count
+  */
+ public int getCount() {
+  return count;
+ }
 
-	public void setCount(int count) {
-		this.count = count;
-	}
+ /**  
+  *  Function to set count
+  */
+ public void setCount(int count) {
+  this.count = count;
+ }
 
 }
