7fbbaf159bdc474b882546deb0a683a6f6a9aa73
Amal Krishna <amalkrishnat94@gmail.com>
2016-05-05 21:26:23 +0530

    Updated Commit

--- src/main/java/com/wc/util/JSONService.java
+++ src/main/java/com/wc/util/JSONService.java
@@ -1,24 +1,33 @@
+ /*
+ * JSONService.java
+ * 
+ * v1.0
+ *
+ * 20 April 2016
+ */
+
 package com.wc.util;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.QueryParam;
 
 @Path("/")
 public class JSONService {
 
-	@GET
-	@Produces("application/json")
-	public CountResponse getProductInJSON(@QueryParam("query") String word) {
-		
-		int c = CountCalculator.countWord(word);
-		CountResponse cr = new CountResponse();
-		cr.setCount(c);
-		
-		return cr;
-	}
-	
+ @GET
+ @Produces("application/json")
+ /**  
+  *  Function to call the countWord function and retun JSON as response
+  */
+ public CountResponse getProductInJSON(@QueryParam("query") String word) {
+
+  /*Fetch the input from ?query param and send to countWord*/
+  int c = CountCalculator.countWord(word);	
+  CountResponse cr = new CountResponse();
+  cr.setCount(c);
+
+  return cr;
+ }
 }
