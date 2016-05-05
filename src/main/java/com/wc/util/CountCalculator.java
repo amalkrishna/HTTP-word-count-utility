25cb8bdcc2ca8c54dbbb5940aefe69b49e1b31aa
Amal Krishna <amalkrishnat94@gmail.com>
2016-05-05 21:19:20 +0530

    Revert "Updated Commit"
    This reverts commit 3aec1ba72db251ce7c76ea84679d82bc5930e1e8.

--- src/main/java/com/wc/util/CountCalculator.java
+++ src/main/java/com/wc/util/CountCalculator.java
@@ -1,62 +1,33 @@
-/*
- * CountCalcuator.java
- * 
- * v1.0
- *
- * 20 April 2016
- */
-
 package com.wc.util;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
+import java.util.*;
 
 public class CountCalculator {
 
- static File file = null;
- private static List < String > wordlist = null;
- 
- /**  
-  *  Function to count the word that is queried
-  */
- public static int countWord(String word) {
-  int count = 0;                            //Count Variable
-  readData();                               //Calling Function readData
-  if (wordlist != null) {
-   try {
-    for (Object obj: wordlist) {
-     String element = (String) obj;
-     if (element.equalsIgnoreCase(word)) {
-      /*Increment count if the word is found*/
-      count++;                              
-     }
-    }
-   } catch (Exception e) {
-    System.out.println(e);
-   }
-  }
-  return count;                             //Return count
- }
+	static File file = null;
 
- /**  
-  *
-  *  Function to read the file and cache the content 
-  */
- public static void readData() {
-  if (wordlist == null) {
-   try {
-    wordlist = new ArrayList < String > ();  //ArrayList 
-    file = new File("D://Java/test.txt");    //Open the input file
-    Scanner scanner = new Scanner(file);     
-    while (scanner.hasNext()) {
-     wordlist.add(scanner.next());           //Append the content to ArrayList
-    }
-    scanner.close();
-   } catch (FileNotFoundException e) {
-    System.out.println(e);
-   }
-  }
- }
+	public static int countWord(String word) 
+	{
+		int count = 0;
+		try
+		{
+			file=new File("D://Java/test.txt");
+			Scanner scanner = new Scanner(file);
+			while (scanner.hasNextLine())
+			{
+    			String nextToken = scanner.next();
+    			if (nextToken.equalsIgnoreCase(word))
+    			{
+    				count++;
+    			}
+			}
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println(e);
+		}
+		return count;
+	}	
 }
--- src/main/java/com/wc/util/CountResponse.java
+++ src/main/java/com/wc/util/CountResponse.java
@@ -1,28 +1,15 @@
-/*
- * CountResponse.java
- * 
- * v1.0
- *
- * 5 May 2016
- */
-
 package com.wc.util;
 
 public class CountResponse {
 
- int count;
- /**  
-  *  Function to return the count
-  */
- public int getCount() {
-  return count;
- }
+	int count;
+	
+	public int getCount() {
+		return count;
+	}
 
- /**  
-  *  Function to set count
-  */
- public void setCount(int count) {
-  this.count = count;
- }
+	public void setCount(int count) {
+		this.count = count;
+	}
 
 }
